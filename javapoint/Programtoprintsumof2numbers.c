2802e4080bad2d753bf3c1b3732618195dae78fc
krishnajhapate <ki54.jhapate@gmail.com>
2021-10-19 23:10:45 +0530

    Program to print sum of 2 numbers

---
+++ javapoint/Programtoprintsumof2numbers.c
@@ -0,0 +1,12 @@
+// Program to print sum of 2 numbers
+
+#include<stdio.h>
+int main (){
+    int a , b ;
+    printf("ENter number a ");
+    scanf("%d",&a);
+    printf("ENter number b ");
+    scanf("%d",&b);
+    printf("SUm of number is %d\n",a+b);
+    return 0;
+}
