62cf69999a9cecfb8f65ad14d40e34f496932687
Krishna jhapate <krishnajhapate@Krishnas-MacBook-Air.local>
2021-11-10 09:07:24 +0530

    node deined

---
+++ teacher/linkedList.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+
+// Defining a node for linked list
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Creating Node for linked list
+struct Node * createNode()
+{
+    struct Node *temp;
+    temp
+};
+
