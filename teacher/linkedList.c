989d713d4306d38e42182f385dabc8b015018618
Krishna jhapate <krishnajhapate@Krishnas-MacBook-Air.local>
2021-11-10 09:25:17 +0530

    node created

--- teacher/linkedList.c
+++ teacher/linkedList.c
@@ -1,6 +1,7 @@
-
+// This program demonstrate the implimentation of linked list
+// Inseting the header file
 #include <stdio.h>
-
+#include <stdlib.h>
 
 // Defining a node for linked list
 struct Node
@@ -10,9 +11,10 @@ struct Node
 };
 
 // Creating Node for linked list
-struct Node * createNode()
+struct Node *createNode()
 {
     struct Node *temp;
-    temp
+    temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->next = NULL;
+    return (temp);
 };
-
