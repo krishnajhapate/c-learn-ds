9d6da0306614189d030d724ce359c23c4f867878
Krishna jhapate <krishnajhapate@Krishnas-MacBook-Air.local>
2021-11-11 13:27:47 +0530

    added node at last

--- teacher/linkedList.c
+++ teacher/linkedList.c
@@ -18,3 +18,50 @@ struct Node *createNode()
     temp->next = NULL;
     return (temp);
 };
+
+// Inserting node at the begining
+struct Node *addNodeFirst(struct Node *head, int value)
+{
+    struct Node *temp;
+    temp = createNode();
+    temp->data = value;
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        temp->next = head;
+        head = temp;
+    }
+    return (head);
+}
+
+// Inserting Node at the last
+struct Node *addNodeLast(struct Node *head, int value)
+{
+    struct Node *temp, *p;
+    temp = createNode();
+    temp->data = value;
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        p = head;
+        while (p->next != NULL)
+        {
+            p = p->next;
+            p->next = temp;
+        }
+    }
+    return (head);
+}
+
+int main()
+{
+    printf("%s", createNode());
+    printf("Hello world");
+    return 0;
+}
